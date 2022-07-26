28adbdcc43d7ecf474dde6a2d6bb3b570edc3d38
JustinA0628 <justin.angara@gmail.com>
2022-07-26 05:38:11 -0500

    Added CRUD

--- index.js
+++ index.js
@@ -1,34 +1,173 @@
+
+// creates config
 let config = {
   draggable: true,
   position:'start'
 }
-let board = ChessBoard('board1',config);
 
+// creates chess board with proper configuration(start pos)
+let board = ChessBoard('board1',config);
+// add event listener to reset the board
 $('#restartBtn').on('click',board.start);
 
-searchBtn.addEventListener("click",function(){
+
+$('#searchBtn').on('click',function(){
   board.position($('#searchbar').val());
+  $('#searchbar').val("");
+});
+
+$('#addBtn').on('click',function(){
+  CrudManager.createFen($('#enterName').val(),$('#enterFen').val());
+  $('#enterName').val("");
+  $('#enterFen').val("");
+});
+
+$('#copyBtn').on('click',function(){
+  let text = board.fen();
+  navigator.clipboard.writeText(text).then(() => {
+    console.log("copied");  
+  });
 });
 
 
+class CrudManager {
+  // crudcrud url
+  static url = "5fa8a0e76d89420da73b7d5e63ad566c"; 
 
+  // creates name to fens
+  static createFen(name,fen){
+    fetch('https://crudcrud.com/api/'+this.url+'/fens', {
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        fen: fen
+      })
 
-let index =0;
-let fenArr=[];
-function addPosition(name,fen){
-  let temp=`<option id="b${index}"class="dropdown-item" value = ${index} onclick="changeText(${index})" > ${name} </option>`;
-  $('#dropdownpos').append(temp);
-  fenArr[index]=fen;
-  index++;
+    })
+    .then(response => response.json())
+    .then(data =>{
+      console.log(data)
+      this.getTable()
+    })
+  }
+  
+  static getTable(){
+    console.log("reset");
+    $.ajax({
+      url: 'https://crudcrud.com/api/'+this.url+'/fens',
+      method: 'GET',
+      dataType: "json",
+      success: function(data){
+        $('#table-body').empty();
+        $(data).each(function(index, element){
+          addRows(index,element);
+        });
+      }
+
+    });
+    
+  }
+  
+  // deletes fen given the id
+  static deleteFen(id){
+    $.ajax({
+      url: 'https://crudcrud.com/api/'+this.url+'/fens/'+id,
+      method: 'DELETE',
+      success: function(){
+        console.log("DELETED");
+        $('#table-body').fadeOut(200);
+        CrudManager.getTable();
+      },
+      error: function(error){
+        console.log(error);
+      }
+    })
+  }
+
+  static updateFen(id,name,fen){
+    fetch(
+      'https://crudcrud.com/api/'+this.url+'/fens/'+id, {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'PUT',
+        body: JSON.stringify({
+          name: name,
+          fen: fen
+        })
+      })
+      .then(response => {
+        this.getTable();
+        console.log(response);
+      })
+  }
 
 }
-addPosition("Italian Game","r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3");
-addPosition("Ruy Lopez","r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3");
-addPosition("Sicilian Defense","rnbqkbnr/pp2pppp/3p4/2p5/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq d3 0 3");
 
-function changeText(x){
-  console.log(x);
-  $('#searchbar').val(fenArr[x]);  
+function addRows(a,b){
+
+  $('#table-body').fadeIn(1000);
+  $('#table-body').html(
+    $('#table-body').html()+
+    `
+      <tr id="${a}">
+        <td class="index" value = ${b._id}>${a}</td>
+        <td>${b.name}</td>
+        <td>${b.fen}</td>
+
+        <td>
+          <button value="${b._id}+${b.fen}:${a}" type="button" class="btn btn-success">Play</button>
+        </td>
+        <td>
+          <button value="${b._id}+${b.fen}:${a}" type="button" class="btn btn-secondary">Update</button>
+        </td>
+        <td>
+          <button value="${b._id}+${b.fen}:${a}" type="button" class="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    `);
+    
+
 }
 
+
+CrudManager.getTable();
+
+
+// wait til all is fetched
+$(document).ajaxStop(function(){
+  // add button listener
+  const buttons = document.querySelectorAll('.btn')
+  buttons.forEach(function(currentBtn){
+    currentBtn.addEventListener('click', event=>{
+
+      let element = event.target;
+
+      // returns fen given a type of string: name+fen:row
+      let fen = element.value.slice(element.value.indexOf("+")+1);
+      fen = fen.slice(0,-2);
+      // returns id 
+      let id = element.value.substring(0,element.value.indexOf("+"))
+
+      // return row #
+      let row = element.value.slice(element.value.indexOf(":")+1);
+
+
+      console.log("ROW:"+row);
+      if(element.innerHTML=="Play"){
+
+        $('#searchbar').val(fen);
+        board.position($('#searchbar').val());
+
+      } else if(element.innerHTML=="Delete"){
+
+        CrudManager.deleteFen(id);
+        
+      } else{
+        // update
+        CrudManager.updateFen(id,$('#enterName').val(),$('#enterFen').val())
+      }
+
+    });
+  });
+});
 
